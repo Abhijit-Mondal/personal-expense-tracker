8d1f4d9072c7666e27f749258ad10a5a8ad55ebf
Abhijit Mondal <abhijitmondal1912@gmail.com>
2022-05-14 16:03:43 +0530

    Temporary fix 13: Delete Expense Button

--- public/js/app.js
+++ public/js/app.js
@@ -11,10 +11,10 @@ for (let deleteBtn of deleteExpenseBtns) {
         let id = evt.target.id;
         let baseurl = evt.target.attributes.baseurl.value;
         let deleteUrl = `${baseurl}/${id}`;
+        console.log(deleteUrl);
         fetchRequests.deleteResource(deleteUrl)
             .then(resultData => {
                 console.log(resultData);
-                alert("Deleted!");
                 location.reload();
             })
             .catch(error => console.error(error));
