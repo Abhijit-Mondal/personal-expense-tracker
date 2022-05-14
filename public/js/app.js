45b8db1bd26c6710dae4b99d7e227056b2cf8cf3
Abhijit Mondal <abhijitmondal1912@gmail.com>
2022-05-14 14:39:21 +0530

    Temporary fix 3: Delete Expense Button

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,12 @@ const fetchRequests = new FetchRequests();
 
 for (let deleteBtn of deleteExpenseBtns) {
 
-    deleteBtn.addEventListener("click", async (evt) => {
+    deleteBtn.addEventListener("click", (evt) => {
         evt.preventDefault();
         let id = evt.target.id;
         let baseurl = evt.target.attributes.baseurl.value;
         let deleteUrl = `${baseurl}/${id}`;
-        await fetchRequests.deleteResource(deleteUrl)
+        fetchRequests.deleteResource(deleteUrl)
             .then(resultData => {
                 console.log(resultData);
                 location.reload();
